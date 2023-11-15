40eed6e711f9f710cc4150f6d536cae2d4b37f01
towhidul-islam-dev <niamaaktar@gmail.com>
2023-11-16 02:04:04 +0600

    added all neccessary dependancies

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+export default function Home() {
+  return (
+    <h2>
+      making a pinterest clone.
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
+    </h2>
+  )
+}
